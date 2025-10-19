84629b3d2e52c432dc7f388e90d151af59727fbd
Nandanarayaroth <nandanarayaroth7@gmail.com>
2025-10-19 23:40:15 +0530

    thid Commit

--- day4/firsthalf/task10.js
+++ day4/firsthalf/task10.js
@@ -1,15 +1,35 @@
 
-function Validity(str) {
-    let splitted = str.split("")
+// function Validity(str) {
+    
+//     let splitted = str.split("")
 
-    if(splitted.includes('@')&&splitted.includes('.')){
-        return true
-    }
-    else{
-        return false
-    }
+//     if(splitted.includes('@')&&splitted.includes('.')){
+//         return true
+//     }
+//     else{
+//         return false
+//     }
+// }
+
+function Validity(str) {
+  if (!str || str.includes(" ")) return false;
+
+  const atIndex = str.indexOf("@");
+  const dotIndex = str.lastIndexOf(".");
+  if (atIndex === -1 || dotIndex === -1) return false;
+
+  if (atIndex > dotIndex) return false;
+
+  if (atIndex === 0 || atIndex === str.length - 1) return false;
+
+  if (dotIndex === 0 || dotIndex === str.length - 1) return false;
+
+  if (dotIndex - atIndex < 2) return false;
+
+  return true;
 }
 
-console.log(Validity("user@domain"));
+console.log(Validity("user@domain"))
+console.log(Validity("user@domain.com"))
 
 // 
